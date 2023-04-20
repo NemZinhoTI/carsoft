23be069d91c181abe54a3daf66e0ad97bc2bf9cf
Amaurilio Alves da Silva <amaurilio60@gmail.com>
2023-04-19 22:07:00 -0300

    commit do projeto de servlet em sala de aula

---
+++ src/main/java/servlet/CreateCarServlet.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import dao.CarDao;
+import model.Car;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/create-car")
+public class CreateCarServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+
+        String carName = req.getParameter("car-name");
+
+
+        Car car = new Car(carName);
+
+        new CarDao().createCar(car);
+
+        resp.sendRedirect("/find-all-cars");
+
+    }
+
+}
